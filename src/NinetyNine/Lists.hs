5328e8d4aea10858a4f9b435a0bbd1e87682d1e1
Spaceface16518 <amrit.rathie@gmail.com>
2022-05-03 13:32:26 -0500

    problem 2

--- src/NinetyNine/Lists.hs
+++ src/NinetyNine/Lists.hs
@@ -1,8 +1,14 @@
 module NinetyNine.Lists
   ( myLast,
+    myButLast,
   )
 where
 
 myLast [] = undefined
 myLast [x] = x
 myLast (x : xs) = myLast xs
+
+myButLast [] = undefined
+myButLast [x] = undefined
+myButLast (x : [_last]) = x
+myButLast (x : xs) = myButLast xs
--- test/Spec.hs
+++ test/Spec.hs
@@ -4,13 +4,19 @@ import Test.HUnit
 main :: IO ()
 main = runTestTTAndExit tests
 
-tests =
+tests = test [lists]
+
+lists =
   test
     [ "lists"
         ~: test
           [ "problem 1"
               ~: [ 4 ~=? myLast [1, 2, 3, 4],
                    'z' ~=? myLast ['x', 'y', 'z']
+                 ],
+            "problem 2"
+              ~: [ 3 ~=? myButLast [1, 2, 3, 4],
+                   'y' ~=? myButLast ['a' .. 'z']
                  ]
           ]
     ]
